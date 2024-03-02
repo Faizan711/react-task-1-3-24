a7364d532f4be92d4726287a9d4a6494bbcae9ad
Faizan <faizanalam980@gmail.com>
2024-03-02 13:46:19 +0530

    added all components in one place

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import WeatherDisplay from './components/WeatherDisplay';
+import RecentSearches from './components/RecentSearches';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [weather, setWeather] = useState(null);
+  const [searchHistory, setSearchHistory] = useState([]);
+
+  const handleSearch = async (city) => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=b41164dd51462550102fa04308bd6b74`
+      );
+      setWeather({
+        city: response.data.name,
+        temperature: response.data.main.temp,
+        conditions: response.data.weather[0].description,
+        windSpeed: response.data.wind.speed,
+      });
+      setSearchHistory((prevHistory) => [city, ...prevHistory.slice(0, 4)]);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      Alert('Error fetching weather data:', error)
+      // Handle error (display a message to the user)
+    }
+  };
 
   return (
     <>
+      <h1>Weather Help</h1>
+      <p>Get Info for any place you want!</p>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar onSearch={handleSearch} />
+      {weather && <WeatherDisplay weather={weather} />}
+      <RecentSearches history={searchHistory} onSearch={handleSearch} />
+    </div>
     </>
   )
 }
